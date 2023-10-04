7c843f2360309d98d8a228deb7e8efed61fd0dc8
joel jimenez <joel_jimenez@live.com>
2023-10-03 20:24:49 -0400

    searchbox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,7 @@ import { AboutPageComponent } from './shared/pages/about-page/about-page.compone
 import {  ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 const routes:Routes =[
-  {
-    path:'',
-    component: HomePageComponent
-  },
+
   {
     path:'about',
     component: AboutPageComponent
@@ -19,9 +16,14 @@ const routes:Routes =[
     component: ContactPageComponent
   },
   {
+    path:'countries',
+    loadChildren: ()=> import('./countries/countries.module').then( m =>m.CountriesModule )
+  },
+  {
     path:'**',
-    redirectTo: ''
-  }
+    redirectTo:'countries'
+  },
+
 ]
 
 @NgModule({
